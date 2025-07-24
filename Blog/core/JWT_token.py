dfe7736951f006694c52b3a711d56497d48a4de1
Umar Zulfiqar <umarzulfiqar24@gmail.com>
2025-07-24 16:59:10 +0500

    Extend Expiry date

--- Blog/core/JWT_token.py
+++ Blog/core/JWT_token.py
@@ -4,14 +4,14 @@ from ..schemas import scheme
 from .config import SECRET_KEY
 
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_Days = 7
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(days=ACCESS_TOKEN_EXPIRE_Days)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
